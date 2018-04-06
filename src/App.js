70e8b2b0e4a43016396a402f45351102e40f0630
Eric Fuhrmann <ekfuhrmann@gmail.com>
2018-04-06 12:53:14 -0400

    Updated Animate project

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <AnimateCC animationName="lishtm5" />
+        <AnimateCC animationName="approachOne" />
       </div>
     );
   }
