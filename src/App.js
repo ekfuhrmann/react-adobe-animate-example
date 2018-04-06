dafb36d6fde9ced560ccb2c84cff6982efd21a9d
Eric Fuhrmann <ekfuhrmann@gmail.com>
2018-04-06 11:47:29 -0400

    Initial upload for adobe animate error

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import AnimateCC from 'react-adobe-animate';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AnimateCC animationName="lishtm5" />
+      </div>
+    );
+  }
+}
+
+export default App;
